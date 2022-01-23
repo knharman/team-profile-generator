5b54d8509b306b0fba5ac612d5a18eecb9504aae
Waffles <knharman@gmail.com>
2022-01-23 15:43:44 -0800

    Bootsrap and CSS styling

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -74,12 +74,13 @@ const generateHTML = function(answers) {
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+            <link rel="stylesheet" href="./styles.css">
             <title>My Team</title>
         </head>
         <body>
-            <nav class="navbar navbar-light bg-light">
-                <div class="container-fluid">
-                <span class="navbar-brand mb-0 h1">My Team</span>
+            <nav class="navbar navbar-light">
+                <div class="container-fluid" id="navbarStyle">
+                <span class="navbar-brand mb-0 h1" id="navbarStyle">My Team</span>
                 </div>
             </nav>
             <div class="container">
